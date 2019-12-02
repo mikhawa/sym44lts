eab5952add08b78f7cce716e837eb1ef4cc543fb
mikhawa <michaeljpitz@gmail.com>
2019-12-02 10:08:52 +0100

    UserFixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,9 +10,18 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // création d'une instance de Entity/User
+        $user = new User();
 
+        // utilisation des setters pour remplir l'instance
+        $user->setThelogin("Lulu")
+            ->setThename("Lulu Poilu")
+            ->setThepwd("Lulu");
+
+        // on sauvegarde l'utilisateur dans doctrine
+        $manager->persist($user);
+
+        // doctrine enregistre l'utilisateur dans la table user
         $manager->flush();
     }
 }
