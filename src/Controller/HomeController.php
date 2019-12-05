0187319c874290f368a7ab21dcc3295f30e61e99
mikhawa <michaeljpitz@gmail.com>
2019-12-05 11:18:07 +0100

    findall d'articles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 // nécessaire pour la requête du menu
 use App\Entity\Categ;
+// nécessaire pour les articles
+use App\Entity\Article;
 
 class HomeController extends AbstractController
 {
@@ -18,17 +20,21 @@ class HomeController extends AbstractController
         // Doctrine récupère tous les champs de la table Categ
         $recupMenu = $this->getDoctrine()->getRepository(Categ::class)->findAll();
 
+        // Doctrine récupère les articles
+        $recupArticles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
         //dump($recupMenu);
 
         // chargement du template
         return $this->render('home/index.html.twig', [
             // envoi du résultat de la requête à twig sous le nom "suitemenu"
             "suitemenu"=>$recupMenu,
+            "articles"=>$recupArticles,
         ]);
     }
     /**
- * @Route("/categ/{slug}", name="categ")
- */
+    * @Route("/categ/{slug}", name="categ")
+    */
     public function detailCateg($slug){
         return new Response($slug);
     }
